4198cce2ebe2f02a64e403fb7ae8cf37154b36ee
AhmedRamadan <ahmedramadanmohamedoweis@gmail.com>
2025-01-30 23:30:47 +0200

    Edit some Module

--- selectDB.sh
+++ selectDB.sh
@@ -2,20 +2,6 @@
 . tableList.sh
 Select_DB() {
     
-    # DBName=$(whiptail --title "Select Database" --inputbox "Please Enter Database Name:" 10 60 3>&1 1>&2 2>&3)
-
-    # if [[ $? -ne 0 || -z "DataBase/$DBName" ]]; then
-    #     whiptail --title "Error" --msgbox "Operation canceled or no name entered." 10 60
-    #     return
-    # fi   
-    # if [[ -d "DataBase/$DBName" ]]; then
-    #     cd "DataBase/$DBName" || exit  
-    #     whiptail --title "Success" --msgbox "Database '$DBName' is connected successfully." 10 60
-    # else
-    #     whiptail --title "Error" --msgbox "Database '$DBName' does not exist." 10 60
-    # fi
-
-
 files=($(ls DataBase))
 
 if [ ${#files[@]} -eq 0 ]; then
